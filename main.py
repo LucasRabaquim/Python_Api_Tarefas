82e34e01e49279d991a0c70a3116aca2d85edb93
Lucas Daniel da Costa Rabaquim <98958822+LucasRabaquim@users.noreply.github.com>
2023-01-31 02:12:35 -0300

    Fix: Mensagens de erro
    Criadas e corrigidas as mensagens de erro e tarefa inexistente respectivamente.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request, session, url_for, redirect
 import json
+import copy
 
 # Id do usuário, o mesmo que está na tarefa que ele criou.
 idUsuario = None
@@ -8,72 +9,76 @@ app = Flask(__name__)
 
 # Chave de Api visível apenas para finalidade de estudo
 app.secret_key = b'1457388d76a3fd5b516cee1af0732442d465f32223f0e14258e1b05e2d320e4f'
-
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == 'POST':
-        session['username'] = request.form['username']
-        idUsuario = session['username']
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+        idUsuario = session["username"]
         return "logado"
-    return '''
+    return """
         <form method="post">
             <p>Usuario: <input type=text name=username>
             <p>Entrar <input type=submit value=Login>
         </form>
-    '''
+    """
 
-@app.route('/logout')
+@app.route("/logout")
 def logout():
-    session.pop('username', None)
+    session.pop("username", None)
     idUsuario = None
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
 
-jsonFile = open('./data.json')
+jsonFile = open("./data.json")
 listaTarefa = json.loads(jsonFile.read())
 
-@app.route('/')
+@app.route("/")
 def index():
-    if 'username' in session:
+    if "username" in session:
         return f'Você logou como: {session["username"]} = {idUsuario}'
-    return 'Você não está logado(a)'   
+    return "Você não está logado(a)"
 
-@app.route('/tarefa',methods=['GET'])
+@app.route("/tarefa", methods=["GET"])
 def select_all():
-    listaTarefaShow = listaTarefa
+    listaTarefaShow = copy.deepcopy(listaTarefa)
     for tarefa in listaTarefaShow:
-        del tarefa['userId']
+        del tarefa["userId"]
     return jsonify(listaTarefaShow)
 
-@app.route('/tarefa/<int:id>',methods=['GET'])
+@app.route("/tarefa/<int:id>", methods=["GET"])
 def select_tarefa_by_id(id):
-        for tarefa in listaTarefa:
-            if tarefa.get('id') == id:
-                tarefaShow = tarefa
-                del tarefaShow['userId']
-                return jsonify(tarefaShow)
-            return "Tarefa não encontrada"
+    listaTarefaShow = copy.deepcopy(listaTarefa)
+    for tarefa in listaTarefaShow:
+        if tarefa.get("id") == id:
+            del tarefa["userId"]
+            return jsonify(tarefa)
+    return "Tarefa não encontrada"
 
-@app.route('/tarefa/<int:id>',methods=['PUT'])
+@app.route("/tarefa/<int:id>", methods=["PUT"])
 def update_tarefa_by_id(id):
-        tarefaUpdate = request.get_json()
-        for index,tarefa in enumerate(listaTarefa):
-                if tarefa.get('id') == id:
-                    listaTarefa[index].update(tarefaUpdate)
-                    return jsonify(listaTarefa[index])
-                return "Tarefa não encontrada"
-           
-@app.route('/tarefa',methods=['POST'])
+    tarefaUpdate = request.get_json()
+    for index, tarefa in enumerate(listaTarefa):
+        if tarefa.get("id") == id:
+            if tarefa.get("userId") == idUsuario:
+                listaTarefa[index].update(tarefaUpdate)
+                return jsonify(listaTarefa[index])
+            return "Acesso Negado"
+    return "Tarefa não encontrada"
+
+@app.route("/tarefa", methods=["POST"])
 def insert_tarefa():
-        tarefaInsert = request.get_json();
-        listaTarefa.append(tarefaInsert)
-        return jsonify(listaTarefa)
+    tarefaInsert = request.get_json()
+    listaTarefa.append(tarefaInsert)
+    return jsonify(listaTarefa)
 
-@app.route('/tarefa/<int:id>',methods=['DELETE'])
+@app.route("/tarefa/<int:id>", methods=["DELETE"])
 def delete_tarefa_by_id(id):
-        for index,tarefa in enumerate(listaTarefa):
-                if tarefa.get('id') == id:
-                    del listaTarefa[index]
-                    return "Deletado"
-                return "Tarefa não encontrada"
 
-app.run(port=5000,host='localhost',debug=True)
+    for index, tarefa in enumerate(listaTarefa):
+        if tarefa.get("id") == id:
+            if tarefa.get("userId") == idUsuario:
+                del listaTarefa[index]
+                return "Deletado"
+            return "Acesso Negado"
+    return "Tarefa não encontrada"
+
+app.run(port=5000, host="localhost", debug=True)
